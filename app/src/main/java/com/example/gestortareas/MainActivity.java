95f0c2b11b45490e763a7c77d622e740965225bc
Alonso-git-max <alonsoleivaroco@gmail.com>
2025-10-02 23:16:22 -0300

    Vistas para aplicacion realizada

--- app/src/main/java/com/example/gestortareas/MainActivity.java
+++ app/src/main/java/com/example/gestortareas/MainActivity.java
@@ -1,24 +1,51 @@
 package com.example.gestortareas;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int CREATE_TASK_REQUEST = 1;
+    private ArrayList<String> tasks;
+    private ArrayAdapter<String> tasksAdapter;
+    private ListView listViewTasks;
+    private Button buttonAddTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        tasks = new ArrayList<>();
+        listViewTasks = findViewById(R.id.listViewTasks);
+        buttonAddTask = findViewById(R.id.buttonAddTask);
+
+        tasksAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, tasks);
+        listViewTasks.setAdapter(tasksAdapter);
+
+        buttonAddTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PantallaCrearTarea.class);
+                startActivityForResult(intent, CREATE_TASK_REQUEST);
+            }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CREATE_TASK_REQUEST && resultCode == RESULT_OK && data != null) {
+            String taskName = data.getStringExtra("taskName");
+            String taskDescription = data.getStringExtra("taskDescription");
+            if (taskName != null) {
+                tasks.add(taskName + ": " + taskDescription);
+                tasksAdapter.notifyDataSetChanged();
+            }
+        }
     }
 }
